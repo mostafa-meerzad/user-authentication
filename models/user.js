410bf9ccdc7ccf4c619ba62ea8564dc4c249cb84
mostafa <mostafameerzad@gmail.com>
2024-01-02 17:55:08 +0430

    create user model for storing users in the DB

---
+++ models/user.js
@@ -0,0 +1,42 @@
+const Joi = require("joi");
+const mongoose = require("mongoose");
+// create a user model for storing users to the database
+
+const User = mongoose.model(
+  "User",
+  new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 55,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      minlength: 5,
+      maxlength: 255,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 1024,
+    },
+  })
+);
+
+// validate users data that is being passed from end-users
+
+function validateUser(user) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(55).required(),
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(55).required(),
+  });
+  return schema.validate(user);
+}
+
+module.exports.User = User;
+module.exports.validate = validateUser;
